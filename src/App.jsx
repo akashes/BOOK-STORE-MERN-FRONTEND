a8bdb6d9cdf18a91ef03a869605d679130f5dbca
akash <akashes5753279@gmail.com>
2025-04-21 14:22:14 +0530

    feat:add news section and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
     <Outlet/>
     </main>
 
-    <footer>footer</footer>
+    <Footer/>
     </>
   )
 }
